ffe37113f2ba7a15fc27d9bee55de40ce930cc00
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-12-06 22:27:26 +0700

    add store

---
+++ client/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, compose, applyMiddleware } from 'redux'
+import rootReducer from '../reducers'
+import ReduxLogger from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
+
+export default configureStore = () => {
+  const logger = ReduxLogger()
+
+  const enhancer = compose( applyMiddleware(ReduxThunk, logger) )
+
+  const store = createStore(
+    rootReducer,
+    enhancer
+  )
+
+  return store
+}
